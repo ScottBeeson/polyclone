2b84b8e79e6e38cd84c09ec7aacf980fc23674c7
Scott Beeson <sbeeson@gmail.com>
2014-07-29 11:22:09 -0500

    Now prompts for a description
    replaces any instance of ${DESCRIPTION} in template folder.

--- polyclone.bat
+++ polyclone.bat
@@ -1,19 +1,19 @@
 
 @echo off &setlocal
-set /p seed= Enter Template Folder: 
-set /p target= Enter New Name: 
-@echo Creating new element named "%target%"
-echo Creating Folder...
-md %target%
+set /p template= Enter Template Folder:
+set /p newname= Enter New Name:
+set /p description= Enter Description:
+echo Creating new element named "%newname%"
+md %newname%
 
-for %%i in (%seed%\*.*) do (
+for %%i in (%template%\*.*) do (
 	set "filename=%%~ni"
 	set ex=%%~xi
 
-    if %seed%==%%~ni (
-        call:WriteFile %seed%\%%~ni%%~xi %target%\%target%%%~xi
+    if %template%==%%~ni (
+        call:WriteFile %template%\%%~ni%%~xi %newname%\%newname%%%~xi
     ) else (
-        call:WriteFile %seed%\%%~ni%%~xi %target%\%%~ni%%~xi
+        call:WriteFile %template%\%%~ni%%~xi %newname%\%%~ni%%~xi
     )
 )
 
@@ -23,15 +23,18 @@ GOTO:EOF
 :WriteFile
 echo Writing %~1 to %~2
 set "search=${NAME}"
-set "replace=%target%"
+set "search2=${DESCRIPTION}"
+set "replace=%newname%"
+set "replace2=%description%"
 set "textfile=%~1"
 set "newfile=%~2"
 
 
-(for /f "delims=" %%i in ('findstr "^" "%textfile%"') do (
+(for /f "delims=" %%i in ('findstr "." "%textfile%"') do (
     set "line=%%i"
     setlocal enabledelayedexpansion
     set "line=!line:%search%=%replace%!"
+    set "line=!line:%search2%=%replace2%!"
     echo(!line!
     endlocal
 ))>"%newfile%"
